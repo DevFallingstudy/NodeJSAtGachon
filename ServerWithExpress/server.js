842e63500def10333ae9b925c565187989417f3e
YANG YOU SUNG - DevFallingstar <stardung86@gmail.com>
2018-03-20 14:25:17 +0900

    Add REST API with Express and NodeJs

--- ServerWithExpress/server.js
+++ ServerWithExpress/server.js
@@ -1,5 +1,7 @@
 var express = require('express')
 var app = express();
+var bodyParser = require('body-parser');
+app.use(bodyParser.urlencoded({extended:false}))
 app.use(express.static('public'))
 app.use(function (req, res, next) {
   console.log('Time:', Date.now());
@@ -13,3 +15,43 @@ app.get('/', function (req, res) {
 app.listen(3000, function () {
   console.log('Example app listening on port 3000!')
 })
+
+app.post('/user', function (req, res) {
+  console.log('데이터 확인', req.body);
+
+  res.send({state: 'OK', data: req.body});
+});
+app.get('/user/search', function (req, res) {
+  console.log('데이터 확인', req.query.name)
+
+  if (req.query.name == 'John') {
+    var users = [{
+      userId: 13579,
+      name: 'John',
+      email: 'yohany_AT_gmail.com',
+      company: 'KossLAB'
+    }];
+  }else{
+    var users = [{
+    }];
+  }
+
+  res.send({result: users});
+});
+app.get('/user/:userId', function (req, res) {
+  console.log(req.params.userId + '의 정보를 가져옵니다');
+  var user = {
+    userId: 13579,
+    name: 'Yang',
+    email: 'stardung86@gmail.com',
+    company: 'DevFallingstar'
+  }
+
+  res.send(user)
+});
+app.put('/user/:userId', function (req, res) {
+  res.send('PUT (Update) ')
+});
+app.delete('/user/:userId', function (req, res) {
+  res.send('DELETE (Delete) ')
+});
